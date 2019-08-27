a81ad4ca1acdfef036c272c031ff74b6db48c2f2
Simo Muraja <simo.muraja@aalto.fi>
2019-08-28 01:37:53 +0300

    Update

--- BattleriteBot/Program.cs
+++ BattleriteBot/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
 using BattleriteBot;
-using Telegram.Bot;
 
 namespace Awesome
 {
@@ -8,11 +9,23 @@ namespace Awesome
     {
         static void Main(string[] args)
         {
-            if (!args.Any()) return;
+            Console.WriteLine("Bot started!");
+            var key = "";
+            if (args.Any())
+            {
+                Console.Write("Key found through args.");
+                key = args[0];
+            }
+            else
+            {
+                Console.WriteLine("No args, finding key in file..");
+                key = File.ReadAllText("../../../../key.txt");
+            }
+            Console.WriteLine("Key aquired!\nStarting up..");
             var com = new BotWrapper();
             var handler = new MainController();
             var comPros = new CommandProcessor(com, handler);
-            com.Start(comPros, args[0]);
+            com.Start(comPros, key);
         }
     }
 }
